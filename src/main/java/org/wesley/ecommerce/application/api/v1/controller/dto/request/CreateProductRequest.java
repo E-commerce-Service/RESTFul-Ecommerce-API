2302824940acbfbd6ebd0762b9b6a22f56a691a5
Wesley00s <wesley300rodrigues@gmail.com>
2025-12-01 10:36:04 -0300

    feat: Update CreateProductRequest to use BigDecimal for price

--- src/main/java/org/wesley/ecommerce/application/api/v1/controller/dto/request/CreateProductRequest.java
+++ src/main/java/org/wesley/ecommerce/application/api/v1/controller/dto/request/CreateProductRequest.java
@@ -1,11 +1,13 @@
 package org.wesley.ecommerce.application.api.v1.controller.dto.request;
 
 
+import java.math.BigDecimal;
+
 public record CreateProductRequest(
         String name,
         String description,
         Integer stock,
         String categoryName,
-        Double price
+        BigDecimal price
 ) {
 }
